7d49f4a8ea55c0a06677f2f671b00cef933b6470
Maulana Ishak <98244927+MaulIshak@users.noreply.github.com>
2025-02-10 08:39:44 +0700

    initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+} Node;
+
+void printNodes(Node*);
+
+int main() {
+    Node *head = malloc(sizeof(Node));
+    head->data = 10;
+    head->next = NULL;
+    
+    Node *current = malloc(sizeof(Node));
+    current->data = 98;
+    current->next = NULL;
+    head->next = current;
+    
+    current = malloc(sizeof(Node));
+    current->data = 70;
+    current->next = NULL;
+    head->next->next = current;
+    
+    current = malloc(sizeof(Node));
+    current->data = 69;
+    current->next = NULL;
+    head->next->next->next = current;
+
+    printNodes(head);
+
+    
+    
+    return 0;
+}
+
+void printNodes(Node *head){
+    Node *current = head;
+    while(current->next != NULL){
+        printf("[%d|%p] -> ", current->data, current->next);
+        current = current->next;
+    }
+    printf("[%d|%p]", current->data, current->next);
+}
